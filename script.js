d0f492ae6a26fdd946d744d8527301630790e8d0
Chase Spengler <chasespengler7@gmail.com>
2022-03-02 15:04:13 -0500

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    if (principal <= 0) {
+        alert("Please enter positive number.")
+    }
+    else {
+    document.getElementById("result").innerHTML = "<br />If you deposit " + principal + 
+    ", <br />at an interest rate of " + rate + "%. <br /> You will receive an amount of " +
+    interest + ", <br /> in the year " + year;
+    }
 }
-        
+
+function updateRate() 
+{
+    var ratevale = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=ratevale;
+}
